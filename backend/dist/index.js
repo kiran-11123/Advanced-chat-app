3af057ff602cf0e654397ba1a3c1dfd020323654
Kiran <kiran.sadara6730@gmail.com>
2025-10-05 14:30:39 +0530

    starting the frontend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,10 +2,34 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8081 });
-let allSockets = {};
+let allSockets = [];
 wss.on("connection", (socket) => {
     console.log("New client connected");
     socket.on("message", (message) => {
-        console.log(`Received message: ${message}`);
+        //type , message are the inputs 
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if (parsedMessage.type === "join") {
+            allSockets.push({
+                //@ts-ignore
+                socket,
+                room: parsedMessage.payload.roomId,
+            });
+        }
+        if (parsedMessage.type === "chat") {
+            //const currentUserRoom = allSockets.find((x) =>x.socket === socket);
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                //@ts-ignore
+                if (allSockets[i].socket === socket) {
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room === currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
+        }
     });
 });
